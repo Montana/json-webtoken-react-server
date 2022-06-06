fb1115f4f4fb04f0b60703567c1f3908223d7d88
Montana Mendy <montana@linux.com>
2022-06-06 04:26:06 -0700

    cleanup

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const { ExtractJwt } = require("passport-jwt");
 
 const User = require("../users/User");
-const secret = "that is what I shared yesterday lol";
+const secret = "your secrets";
 
 const localStrategy = new LocalStrategy(function (username, password, done) {
   User.findOne({ username })
